31c4b8fc0012140c4179581b08154ec2e35a991a
Binu <49573910+binuvdev@users.noreply.github.com>
2020-04-03 13:58:16 -0500

    Add files via upload

--- MinNumberOfJumps.java
+++ MinNumberOfJumps.java
@@ -1,20 +1,22 @@
 package com.java.algo;
+
 //Algo Hard #10
 //O(N^2) time | O(N) space
 public class MinNumberOfJumps {
 
 	public static int minNumberOfJumps(int[] array) {
 		int[] jumps = new int[array.length];
-		for(int i=1;i<jumps.length;i++) jumps[i] = Integer.MAX_VALUE;
-		for (int i=1;i<array.length;i++) {
-			for(int j=0;j<i;j++) {
+		for (int i = 1; i < jumps.length; i++)
+			jumps[i] = Integer.MAX_VALUE;
+		for (int i = 1; i < array.length; i++) {
+			for (int j = 0; j < i; j++) {
 				if (array[j] + j >= i) {
 					jumps[i] = Math.min(jumps[j] + 1, jumps[i]);
-				}	
+				}
 			}
 		}
-		
+
 		return jumps[jumps.length - 1];
 	}
-	
+
 }
